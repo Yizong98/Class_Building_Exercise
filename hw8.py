b79b9916f96668056806a2e046b1ef76c7d435c6
Yizong98 <yizong@ucsd.edu>
2018-05-27 20:01:12 -0700

    feat: Finish all methods

--- hw8.py
+++ hw8.py
@@ -55,7 +55,7 @@ def digits(n):
 
     assert isinstance(n, int), "n must be integer"
 
-    assert(n > 0), "n must be positive integer"
+    assert(n >= 0), "n must be non-negative integer"
 
     s = Link.empty
 
@@ -134,8 +134,8 @@ class VendingMachine:
     'Current balance: $7'
     >>> v.vend()
     'You must deposit $3 more.'
-    >>> v.deposit(5)
-    'Current balance: $12' 
+    >>> v.deposit(5)  
+    'Current balance: $12'
     >>> v.vend()
     'Here is your candy and $2 change.'
     >>> v.deposit(10)
@@ -191,11 +191,60 @@ class VendingMachine:
 ##############################################################################
 # Problem 2
 ##############################################################################
+class Beverage:
 
+    def __init__(self, name, hot):
+        self.name = name
+        self.hot = hot
+
+    def __str__(self):
+        if self.hot:
+            return "Hot {0}".format(self.name)
+        else:
+            return "Cold {0}".format(self.name)
+
+
+class Coffee(Beverage):
+    Beverage.hot = True
+
+    def __init__(self, sugars, cream):
+        assert isinstance(sugars, int), "sugars must be integer"
+        assert isinstance(cream, int), "cream must be integer"
+        self.sugars = sugars
+        self.cream = cream
+        self.ingredients = {"sugars": sugars, "creams": cream}
+
+    def __str__(self):
+        return 'Coffee with {0} sugars and {1} creams'.format(self.sugars, self.cream)
+
+
+class Soda(Beverage):
+    Beverage.hot = False
+
+    def __init__(self, is_carbonated):
+        self.is_carbonated = is_carbonated
+
+    def __str__(self):
+        return 'Cold Soda'
+
+
+class Coke(Soda):
+    Soda.hot = False
+    Soda.is_carbonated = True
+
+    def __init__(self, my_favorite):
+        self.my_favorite = my_favorite
+
+    def __str__(self):
+        if self.my_favorite:
+            return 'Cold Coke is my favorite'
+        else:
+            return 'Could not care less for Coke'
 
 ##############################################################################
 # Problem 3
 ##############################################################################
+
 
 def clip_list(s, index):
     """
@@ -217,6 +266,58 @@ def clip_list(s, index):
     good job
     """
     #Your Code Here#
+    #raise IndexError('Index not available')
+    if s == Link.empty:
+        raise IndexError('Index not available')
+    if index == 0:
+        rest, s.rest = s.rest, Link.empty
+        return rest
+    else:
+        return clip_list(s.rest, index - 1)
+
+
+def remove_last_occurrence(lst, elem):
+    """
+    >>> l2 = Link('a', Link('b', Link('c', Link('d'))))
+    >>> remove_last_occurrence(l2, 'b')
+    >>> l2
+    Link(a, Link(c, Link(d)))
+
+    >>> remove_last_occurrence(l2, 'd')
+    >>> l2
+    Link(a, Link(c))
+
+    >>> l3 = Link.empty
+    >>> remove_last_occurrence(l3, 'delete_me')
+    >>> l3 == Link.empty
+    True
+
+    >>> l4 = Link('b', Link('a', Link('b', Link('a', Link('b')))))
+    >>> remove_last_occurrence(l4, 'a')
+    >>> l4
+    Link(b, Link(a, Link(b, Link(b))))
+    """
+    """YOUR CODE GOES HERE"""
+    if lst == Link.empty:
+        return
+    else:
+        i = lst
+        index = 0
+        found_idx = None
+        while i != Link.empty:
+            if i.first == elem:
+                found_idx = index
+            index += 1
+            i = i.rest
+        if found_idx == None:
+            return "No occurence found!"
+
+    def delete_at(s, idx):
+        if idx == 0:
+            return s.rest
+        else:
+            return Link(s.first, delete_at(s.rest, idx - 1))
+    lst.rest = delete_at(lst, found_idx).rest
 
 
 ##############################################################################
@@ -226,6 +327,55 @@ import numpy as np
 # Q 4.1: Custom Queue Implementation
 
 #Your Code Here#
+
+
+class Queue:
+
+    def __init__(self, front, rear, num_elems=0, capacity=5):
+        self.front = front
+        self.rear = rear
+        self.num_elems = num_elems
+        self.capacity = capacity
+        self.data = np.array([None] * capacity, dtype=object)
+
+    def dequeue(self):
+        elem = self.data[self.front]
+        assert(elem != None), "cannot remove empty elements"
+        self.data[self.front] = None
+        self.front = self.front + 1
+        if (self.front == len(self.data)):
+            self.front = 0
+        self.num_elems -= 1
+        return elem
+
+    def enqueue(self, elem):
+        self.data[self.rear] = elem
+        self.num_elems += 1
+        self.rear = self.rear + 1
+        if (self.rear == len(self.data)):
+            self.rear = 0
+        if self.is_full():
+            self.capacity *= 2
+            new_data = np.array([None] * self.capacity, dtype=object)
+            new_data[0:(len(self.data))] = self.data
+            self.data = new_data
+
+    def is_full(self):
+        if self.num_elems == self.capacity:
+            return True
+        return False
+
+    def is_empty(self):
+        if self.num_elems == 0:
+            return True
+        return False
+
+    def print_queue(self):
+        if self.is_empty():
+            print([])
+            return
+        print('[|' + '|'.join("{}".format(k) for k in self.data) + '|]')
+        return
 
 
 # Q 4.2: Cursed Carousel
@@ -259,3 +409,12 @@ def cursed_carousel(n, m):
     5
     '''
     #Your code here#
+    carousel = Queue(0, 0, capacity=n + 1)
+    for i in (np.arange(n) + 1):
+        carousel.enqueue(i)
+    while carousel.num_elems > 0:
+        for i in range(m):
+            if i != (m - 1):
+                carousel.enqueue(carousel.dequeue())
+            else:
+                print(carousel.dequeue())
